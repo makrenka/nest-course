b218831b731b0dd393680a62683d233c9055c430
makrenka <smokrenko79@gmail.com>
2024-01-25 16:26:53 +0300

    add users and roles

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,31 @@
 import { Module } from "@nestjs/common";
 import { SequelizeModule } from "@nestjs/sequelize";
-
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+import { UsersModule } from "./users/users.module";
+import { ConfigModule } from "@nestjs/config";
+import { User } from "./users/users.model";
+import { RolesModule } from "./roles/roles.module";
+import { Role } from "./roles/roles.model";
+import { UserRoles } from "./users/user-roles.model";
 
 @Module({
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+    }),
     SequelizeModule.forRoot({
       dialect: "postgres",
-      host: "localhost",
-      port: 5432,
-      username: "postgres",
-      password: "root",
-      database: "nest-course",
-      models: [],
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      models: [User, Role, UserRoles],
       autoLoadModels: true,
     }),
+    UsersModule,
+    RolesModule,
   ],
 })
 export class AppModule {}
---
+++ src/users/users.model.ts
@@ -0,0 +1,46 @@
+import { ApiProperty } from "@nestjs/swagger";
+import {
+  BelongsToMany,
+  Column,
+  DataType,
+  Model,
+  Table,
+} from "sequelize-typescript";
+import { Role } from "src/roles/roles.model";
+import { UserRoles } from "./user-roles.model";
+
+interface UserCreationAttr {
+  email: string;
+  password: string;
+}
+
+@Table({ tableName: "users" })
+export class User extends Model<User, UserCreationAttr> {
+  @ApiProperty({ example: "1", description: "Уникальный идентификатор" })
+  @Column({
+    type: DataType.INTEGER,
+    unique: true,
+    autoIncrement: true,
+    primaryKey: true,
+  })
+  id: number;
+
+  @ApiProperty({ example: "user@mail.com", description: "Почтовый адрес" })
+  @Column({ type: DataType.STRING, unique: true, allowNull: false })
+  email: string;
+
+  @ApiProperty({ example: "123", description: "Пароль" })
+  @Column({ type: DataType.STRING, allowNull: false })
+  password: string;
+
+  @ApiProperty({ example: "true", description: "Забанен или нет" })
+  @Column({ type: DataType.BOOLEAN, defaultValue: false })
+  banned: boolean;
+
+  @ApiProperty({ example: "За хулиганство", description: "Причина блокировки" })
+  @Column({ type: DataType.STRING, allowNull: true })
+  banReason: string;
+
+  @BelongsToMany(() => Role, () => UserRoles)
+  roles: Role[];
+}
